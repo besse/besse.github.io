983f3f39365faeb6cc0800543f53d0e886441fb2
Jonas Birgersson <jonas.birgersson@regent.se>
2024-12-22 22:06:30 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿function checkPassword() {
+    const password = document.getElementById('password').value;
+    const clueElement = document.getElementById('clue');
+
+    const correctPassword = '3519'; // Change this to your desired password
+
+    if (password === correctPassword) {
+        clueElement.textContent = 'Här är din ledtråd: Titta på baksidan av denna skylt.';
+        clueElement.style.color = 'green';
+
+    } else {
+        clueElement.textContent = 'Koden är fel. Försök igen.';
+        clueElement.style.color = 'red';
+    }
+}
